db3f60fb0fd4a986adb8f96953b9d90b84ca5b1e
Willy Chang <ms70831@gmail.com>
2017-03-16 21:29:53 +0800

    create custom infiniteViewpager view

---
+++ app/src/main/java/com/devwilly/infiniteviewpagerex/ui/InfiniteViewPager.java
@@ -0,0 +1,21 @@
+package com.devwilly.infiniteviewpagerex.ui;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+
+
+/**
+ * Created by Willy on 2017/3/16.
+ */
+
+public class InfiniteViewPager extends ViewPager {
+
+    public InfiniteViewPager(Context context) {
+        super(context);
+    }
+
+    public InfiniteViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
