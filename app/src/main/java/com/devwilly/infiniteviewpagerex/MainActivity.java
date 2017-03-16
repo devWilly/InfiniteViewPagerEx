7b01ac3df03bfa8cb5bf33f69d7b3022dd0c885d
Willy Chang <ms70831@gmail.com>
2017-03-16 21:58:16 +0800

    add imageList in my InfiniteViewPagerAdapter and mock getCount return value

--- app/src/main/java/com/devwilly/infiniteviewpagerex/MainActivity.java
+++ app/src/main/java/com/devwilly/infiniteviewpagerex/MainActivity.java
@@ -1,7 +1,13 @@
 package com.devwilly.infiniteviewpagerex;
 
+import com.devwilly.infiniteviewpagerex.ui.InfiniteViewPager;
+import com.devwilly.infiniteviewpagerex.ui.InfiniteViewPagerAdapter;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +16,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        InfiniteViewPager viewPager = (InfiniteViewPager) findViewById(R.id.infinite_viewpager);
+
+        int pic1 = R.drawable.pic1;
+        int pic2 = R.drawable.pic2;
+        int pic3 = R.drawable.pic3;
+
+        List<Integer> imageList = new ArrayList<>();
+        imageList.add(pic1);
+        imageList.add(pic2);
+        imageList.add(pic3);
+
+        InfiniteViewPagerAdapter adapter = new InfiniteViewPagerAdapter(this, imageList);
+
+
     }
 }
--- app/src/main/java/com/devwilly/infiniteviewpagerex/ui/InfiniteViewPagerAdapter.java
+++ app/src/main/java/com/devwilly/infiniteviewpagerex/ui/InfiniteViewPagerAdapter.java
@@ -1,7 +1,13 @@
 package com.devwilly.infiniteviewpagerex.ui;
 
+import com.devwilly.infiniteviewpagerex.MainActivity;
+
+import android.content.Context;
 import android.support.v4.view.PagerAdapter;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -10,9 +16,27 @@ import android.view.View;
 
 public class InfiniteViewPagerAdapter extends PagerAdapter {
 
+    private List<Integer> mImageList = new ArrayList<>();
+
+    public InfiniteViewPagerAdapter(Context context, List<Integer> imageList) {
+        this.mImageList = imageList;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+
+        if (getRealCount() == 0) {
+            return 0;
+        }
+
+        /**
+         * return max value as {@link InfiniteViewPager} page count
+         */
+        return Integer.MAX_VALUE;
+    }
+
+    public int getRealCount() {
+        return mImageList.size();
     }
 
     @Override
