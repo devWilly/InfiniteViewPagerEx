15f544fc39d86e239f068f935dbb2be835abd938
Willy Chang <ms70831@gmail.com>
2017-03-18 12:00:26 +0800

    offset current item position

--- app/src/main/java/com/devwilly/infiniteviewpagerex/ui/InfiniteViewPager.java
+++ app/src/main/java/com/devwilly/infiniteviewpagerex/ui/InfiniteViewPager.java
@@ -1,6 +1,7 @@
 package com.devwilly.infiniteviewpagerex.ui;
 
 import android.content.Context;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 
@@ -17,5 +18,45 @@ public class InfiniteViewPager extends ViewPager {
 
     public InfiniteViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    @Override
+    public void setAdapter(PagerAdapter adapter) {
+        super.setAdapter(adapter);
+
+        setCurrentItem(0);
+    }
+
+    @Override
+    public void setCurrentItem(int item) {
+        // offset the current item to ensure there is space to scroll
+        setCurrentItem(item, false);
+    }
+
+    @Override
+    public void setCurrentItem(int item, boolean smoothScroll) {
+        if (getAdapter().getCount() == 0) {
+            super.setCurrentItem(item, smoothScroll);
+            return;
+        }
+        item = getOffsetAmount() + (item % getAdapter().getCount());
+        super.setCurrentItem(item, smoothScroll);
+    }
+
+    public int getOffsetAmount() {
+        if (getAdapter().getCount() == 0) {
+            return 0;
+        }
+
+        if (getAdapter() instanceof InfiniteViewPagerAdapter) {
+            InfiniteViewPagerAdapter adapter = (InfiniteViewPagerAdapter) getAdapter();
+            // allow for 100 back cycles from the beginning
+            // should be enough to create an illusion of infinity
+            // warning: scrolling to very high values (1,000,000+) results in
+            // strange drawing behaviour
+            return adapter.getRealCount() * 100;
+        } else {
+            return 0;
+        }
     }
 }
